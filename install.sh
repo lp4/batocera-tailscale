fc5d26a9c73b66a30ed885b8eac7c700fd9dc74a
codecs02-marcher <44030340+codecs02-marcher@users.noreply.github.com>
2025-01-17 02:25:52 +0530

    tailscale_install.sh

--- install.sh
+++ install.sh
@@ -1,117 +1,135 @@
 #!/bin/bash
 
-# URL of the script to download
-SCRIPT_URL="https://github.com/DTJW92/batocera-unofficial-addons/raw/main/app/symlinks.sh"  # URL for symlink_manager.sh
-BATOCERA_ADDONS_URL="https://github.com/DTJW92/batocera-unofficial-addons/raw/main/app/BatoceraUnofficialAddOns.sh"  # URL for batocera-unofficial-addons.sh
-KEYS_URL="https://github.com/DTJW92/batocera-unofficial-addons/raw/main/app/keys.txt"  # URL for keys.txt
-XMLSTARLET_URL="https://github.com/DTJW92/batocera-unofficial-addons/raw/refs/heads/main/app/xmlstarlet"  # URL for xmlstarlet
+arch=""
+echo "Running install script"
+if [[ "$(uname -m)" == "x86_64" ]]; then
+        arch="386"
+fi
+if [[ "$(uname -m)" == "aarch64" ]]; then
+        arch="arm64"
+fi
+if [[ "$(uname -m)" == "aarch32" ]]; then
+        arch="arm"
+fi
+if [[ "$(uname -m)" == "amd64" ]]; then
+        arch="amd64"
+fi
+if [[ "$(uname -m)" == "riscv64" ]]; then
+        arch="riscv64"
+fi
+if [[ "$(uname -m)" == "x86" ]]; then
+        arch="386"
+fi
+# Finding Architecture.
 
-# Destination path to download the script
-DOWNLOAD_DIR="/userdata/system/services/"
-SCRIPT_NAME="symlink_manager.sh"
-SCRIPT_PATH="$DOWNLOAD_DIR/$SCRIPT_NAME"
+case ${arch} in
+  386)
+    arch="386"
+    echo "supported tailscale zip $arch"
+    ;;
+  arm64)
+    arch="arm64"
+    echo "supported tailscale zip $arch"
+    ;;
+  arm)
+    arch="arm"
+    echo "supported tailscale zip $arch"
+    ;;
+  amd64)
+    arch="amd64"
+    echo "supported tailscale zip $arch"
+    ;;
+  riscv64)
+    arch="riscv64"
+    echo "supported tailscale zip $arch"
+    ;;
+  386)
+    arch="386"
+    echo "supported tailscale zip $arch"
+    ;;
+esac
 
-# Destination path for batocera-unofficial-addons.sh and keys.txt
-ROM_PORTS_DIR="/userdata/roms/ports"
-BATOCERA_ADDONS_PATH="$ROM_PORTS_DIR/BatoceraUnofficialAddOns.sh"
-KEYS_FILE="$ROM_PORTS_DIR/keys.txt"
+# Creating temp files
+echo "Creating temp files..."
+rm -rf /userdata/temp
+mkdir -p /userdata/temp
+cd /userdata/temp || exit 1
 
-mkdir -p "$DOWNLOAD_DIR"
+# Dowload tailscale zip as per architecture
+echo "Downloading tailscale for your system........"
+wget -q https://pkgs.tailscale.com/stable/tailscale_1.78.1_$arch.tgz
 
-# Step 1: Download the symlink manager script
-echo "Downloading the symlink manager script from $SCRIPT_URL..."
-curl -L -o "$SCRIPT_PATH" "$SCRIPT_URL"
+# Exctrating Zip Files
+echo "Extracting Files and Creating Tailscale Folders..."
+tar -xf tailscale_1.78.1_$arch.tgz
+cd tailscale_1.78.1_$arch || exit 1
+rm -rf /userdata/tailscale
+mkdir /userdata/tailscale
+mv systemd /userdata/tailscale/systemd
+mv tailscale /userdata/tailscale/tailscale
+mv tailscaled /userdata/tailscale/tailscaled
+cd /userdata || exit 1
+rm -rf /userdata/temp
 
-# Check if the download was successful
-if [ $? -ne 0 ]; then
-    echo "Failed to download the symlink manager script. Exiting."
+echo "Configuring Tailscale service..."
+mkdir -p /userdata/system/services
+rm -rf /userdata/system/services/tailscale
+cat << 'EOF' > /userdata/system/services/tailscale
+#!/bin/bash
+INTERFACE=$(ip -o -4 route show to default | awk '{print $5}')
+
+if ! ip link show "$INTERFACE" > /dev/null 2>&1; then
+    echo "Error: Interface $INTERFACE does not exist." >&2
     exit 1
 fi
 
-# Download base dependencies
-curl -L https://raw.githubusercontent.com/DTJW92/batocera-unofficial-addons/refs/heads/main/app/dep.sh | bash
+CIDDR=$(ip -o -f inet addr show "$INTERFACE" | awk '{print $4}')
 
-# Step 2: Remove the .sh extension
-SCRIPT_WITHOUT_EXTENSION="${SCRIPT_PATH%.sh}"
-mv "$SCRIPT_PATH" "$SCRIPT_WITHOUT_EXTENSION"
-
-# Step 3: Make the symlink manager script executable
-chmod +x "$SCRIPT_WITHOUT_EXTENSION"
-
-# Step 4: Enable the batocera-unofficial-addons-symlinks service
-echo "Enabling batocera-unofficial-addons-symlinks service..."
-batocera-services enable symlink_manager
-
-# Step 5: Start the batocera-unofficial-addons-symlinks service
-echo "Starting batocera-unofficial-addons-symlinks service..."
-batocera-services start symlink_manager &>/dev/null &
-
-# Step 6: Download batocera-unofficial-addons.sh
-echo "Downloading Batocera Unofficial Add-Ons Launcher..."
-curl -L -o "$BATOCERA_ADDONS_PATH" "$BATOCERA_ADDONS_URL"
-
-if [ $? -ne 0 ]; then
-    echo "Failed to download batocera-unofficial-addons.sh. Exiting."
+if [ -z "$CIDDR" ]; then
+    echo "Error: No IP address found for interface $INTERFACE." >&2
     exit 1
 fi
 
-# Step 7: Make batocera-unofficial-addons.sh executable
-chmod +x "$BATOCERA_ADDONS_PATH"
+IP=$(echo "$CIDDR" | cut -d'/' -f1)
+PREFIX=$(echo "$CIDDR" | cut -d'/' -f2)
 
-# Step 8: Download keys.txt
-echo "Downloading keys.txt..."
-curl -L -o "$KEYS_FILE" "$KEYS_URL"
+MASK=$(( 0xFFFFFFFF << (32 - PREFIX) & 0xFFFFFFFF ))
+MASK_OCTETS=$(printf "%d.%d.%d.%d" $(( (MASK >> 24) & 0xFF )) \
+                                  $(( (MASK >> 16) & 0xFF )) \
+                                  $(( (MASK >> 8) & 0xFF )) \
+                                  $(( MASK & 0xFF )))
 
-if [ $? -ne 0 ]; then
-    echo "Failed to download keys.txt. Exiting."
-    exit 1
+IFS=. read -r o1 o2 o3 o4 <<< "$IP"
+IFS=. read -r m1 m2 m3 m4 <<< "$MASK_OCTETS"
+NETWORK=$(printf "%d.%d.%d.%d" $(( o1 & m1 )) \
+                              $(( o2 & m2 )) \
+                              $(( o3 & m3 )) \
+                              $(( o4 & m4 )))
+
+CIDR=$(printf $NETWORK/$PREFIX)
+
+if [[ "$1" != "start" ]]; then
+  exit 0
 fi
+/userdata/tailscale/tailscaled -state /userdata/tailscale/state > /userdata/tailscale/tailscaled.log 2>&1 &/userdata/tailscale/tailscale up --advertise-routes=$CIDR --snat-subnet-routes=false --accept-routes
 
-# Step 9: Rename keys.txt to match the .sh file name with .sh.keys extension
-RENAME_KEY_FILE="${BATOCERA_ADDONS_PATH}.keys"
-echo "Renaming $KEYS_FILE to $RENAME_KEY_FILE..."
-mv "$KEYS_FILE" "$RENAME_KEY_FILE"
+EOF
+echo "Creating tun, forwarding ip and saving batocera-overlay....."
+rm -rf /dev/net
+mkdir -p /dev/net
+mknod /dev/net/tun c 10 200
+chmod 600 /dev/net/tun
+cp /etc/sysctl.conf /etc/sysctl.conf.bak
+cat <<EOL > "/etc/sysctl.conf"
+net.ipv4.ip_forward = 1
+net.ipv6.conf.all.forwarding = 1
+EOL
 
-# Step: Download xmlstarlet
-echo "Downloading xmlstarlet..."
-curl -L -o "/userdata/system/add-ons/.dep/xmlstarlet" "$XMLSTARLET_URL"
+batocera-save-overlay
+sysctl -p /etc/sysctl.conf
 
-# Check if download was successful
-if [ $? -ne 0 ]; then
-    echo "Failed to download xmlstarlet. Exiting."
-    exit 1
-fi
+# Start Tailscale daemon
+/userdata/tailscale/tailscaled -state /userdata/tailscale/state > /userdata/tailscale/tailscaled.log 2>&1 &/userdata/tailscale/tailscale up
 
-# Make xmlstarlet executable
-chmod +x /userdata/system/add-ons/.dep/xmlstarlet
-
-# Step: Symlink xmlstarlet to /usr/bin
-echo "Creating symlink for xmlstarlet in /usr/bin..."
-ln -sf /userdata/system/add-ons/.dep/xmlstarlet /usr/bin/xmlstarlet
-
-echo "xmlstarlet has been installed and symlinked to /usr/bin."
-mkdir -p "/userdata/roms/ports/images"
-# Step 10: Refresh the Ports menu
-echo "Refreshing Ports menu..."
-curl http://127.0.0.1:1234/reloadgames
-
-# Ensure the gamelist.xml exists
-if [ ! -f "/userdata/roms/ports/gamelist.xml" ]; then
-    echo '<?xml version="1.0" encoding="UTF-8"?><gameList></gameList>' > "/userdata/roms/ports/gamelist.xml"
-fi
-
-# Download the image
-echo "Downloading Batocera Unofficial Add-ons logo..."
-curl -L -o /userdata/roms/ports/images/BatoceraUnofficialAddons.png https://github.com/DTJW92/batocera-unofficial-addons/raw/main/app/extra/batocera-unofficial-addons.png
-echo "Adding logo to Batocera Unofficial Add-ons entry in gamelist.xml..."
-xmlstarlet ed -s "/gameList" -t elem -n "game" -v "" \
-  -s "/gameList/game[last()]" -t elem -n "path" -v "./BatoceraUnofficialAddOns.sh" \
-  -s "/gameList/game[last()]" -t elem -n "name" -v "Batocera Unofficial Add-Ons Installer" \
-  -s "/gameList/game[last()]" -t elem -n "image" -v "./images/BatoceraUnofficialAddons.png" \
-  /userdata/roms/ports/gamelist.xml > /userdata/roms/ports/gamelist.xml.tmp && mv /userdata/roms/ports/gamelist.xml.tmp /userdata/roms/ports/gamelist.xml
-
-
-curl http://127.0.0.1:1234/reloadgames
-
-echo
-echo "Installation complete! You can now launch Batocera Unofficial Add-Ons from the Ports menu."
+batocera-services enable tailscale
+batocera-services start tailscale

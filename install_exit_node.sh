f9f1f3f44cf9369464579470e645d28f744d303d
codecs02-marcher <44030340+codecs02-marcher@users.noreply.github.com>
2025-02-02 03:01:47 +0530

    install_exit_node.sh

--- install_exit_node.sh
+++ install_exit_node.sh
@@ -133,3 +133,13 @@ sysctl -p /etc/sysctl.conf
 
 batocera-services enable tailscale
 batocera-services start tailscale
+
+
+# Check if the error is present in dmesg
+if dmesg | grep -q "UDP GRO forwarding is suboptimally configured"; then
+    # Disable Generic Receive Offload (GRO) on eth0
+    ethtool -K eth0 gro off
+    echo "Fixed UDP GRO forwarding issue on eth0"
+fi
+
+/userdata/tailscale/tailscaled -state /userdata/tailscale/state > /userdata/tailscale/tailscaled.log 2>&1 &/userdata/tailscale/tailscale up

4f55b713821282593bb9746aa7d2c9f1388d0fdf
codecs02-marcher <44030340+codecs02-marcher@users.noreply.github.com>
2025-02-02 03:07:29 +0530

    install.sh

--- install.sh
+++ install.sh
@@ -20,6 +20,11 @@ fi
 if [[ "$(uname -m)" == "x86" ]]; then
         arch="386"
 fi
+if [[ "$(uname -m)" == "armv7l" ]]; then
+        arch="arm"
+fi
+
+
 # Finding Architecture.
 
 case ${arch} in
@@ -131,12 +136,10 @@ sysctl -p /etc/sysctl.conf
 # Start Tailscale daemon
 /userdata/tailscale/tailscaled -state /userdata/tailscale/state > /userdata/tailscale/tailscaled.log 2>&1 &/userdata/tailscale/tailscale up
 
-# Check if the error is present in dmesg
-if dmesg | grep -q "UDP GRO forwarding is suboptimally configured"; then
-    # Disable Generic Receive Offload (GRO) on eth0
-    ethtool -K eth0 gro off
-    echo "Fixed UDP GRO forwarding issue on eth0"
-fi
+
+NETDEV=$(ip -o route get 8.8.8.8 | cut -f 5 -d " ")
+ethtool -K $NETDEV rx-udp-gro-forwarding on rx-gro-list off
+ethtool -K $NETDEV gro off
 
 /userdata/tailscale/tailscaled -state /userdata/tailscale/state > /userdata/tailscale/tailscaled.log 2>&1 &/userdata/tailscale/tailscale up
 
